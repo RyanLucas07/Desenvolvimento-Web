dff1a96993957a5fb7ac86c5791729c2fe4fb38b
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-05-19 08:08:52 -0300

    Adicionando a aula05

---
+++ Aulas/Aula05/js/operadores3.js
@@ -0,0 +1,19 @@
+var numero1 = 5
+var numero2 = 10
+
+var ehMaior = numero1 > numero2
+var ehMenor = numero1 < numero2
+var ehIgual = numero1 == numero2
+var ehMenorOuIgual = numero1 <= numero2
+var ehMaiorOuIgual = numero1 >= numero2
+var ehDiferente = numero1 != numero2
+
+var saoIguais = 10 == "10" //true (verifica valor)
+var saoIdenticos = 10 === "10" // false (verifica tipo e valor)
+
+alert(ehMaior)
+alert(ehMenor)
+alert(ehIgual)
+alert(ehDiferente)
+alert(ehMaiorOuIgual)
+alert(ehMenorOuIgual)
---
+++ Aulas/Aula05/js/template_string.js
@@ -0,0 +1,5 @@
+var nome = "Fulano"
+var idade = 10
+var endereco = "Rua XPTO"
+
+console.log(`Seu nome ´r ${nome} você tem ${idade} anos e mora no endereco ${endereco}`)
