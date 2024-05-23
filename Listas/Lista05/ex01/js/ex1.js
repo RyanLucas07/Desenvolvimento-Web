efce172dde411930b69d7a1ca5e0717c51ae6f98
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-05-23 20:09:10 -0300

    Lista05

---
+++ Listas/Lista05/ex01/js/ex1.js
@@ -0,0 +1,7 @@
+var nome = prompt("Digite seu nome")
+var idade = Number(prompt("Digite seu idade"))
+var empregoAtual = prompt("Digite seu emprego atual")
+var endereco = prompt("Digite seu endereço")
+
+alert(`ola ${nome} \ntudo bem?`)
+alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atul é: ${empregoAtual} \nE mora no endereço ${endereco}`)
---
+++ Listas/Lista05/ex02/ex02.js
@@ -0,0 +1,6 @@
+var nome = "Ryan"
+var idade = 20
+var empregoAtual = "Fui promovido a cliente kkkkkkkkkkkkkkkkkkkkk"
+var endereco = "Rua algum lugar ae"
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoAtual} \nE mora no endereço: ${endereco}`)
