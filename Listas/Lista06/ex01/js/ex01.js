af6c694879ff9e4d24c5957b9cb8e8f4e27e0200
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-05-31 19:13:56 -0300

    Lista06

---
+++ Listas/Lista06/ex01/js/ex01.js
@@ -0,0 +1,37 @@
+var pix = {
+    chavePix: '111.222.333-45',
+    valorDaTranferencia: 100000.00,
+    dataDaTransacao: '26/12/2023',
+    descricracaoDoPagamento: 'Te amo meu lindo!',
+    pagador: [
+        {
+            nome: 'Jair Bolsonaro',
+            documento: '333.444.555-67'
+        }
+    ],
+    recebedor: [
+        {
+            nome: 'Luiz Inacio Lula da Silva',
+            documento: '111.222.333-45'
+        }
+    ],
+    historicoTransacoes: [
+        {
+            data: '26/12/2023',
+            valor: 100000.00,
+            descricracaoDoPagamento: 'Te amo meu lindo!'
+        },
+        {
+            data: '20/12/2023',
+            valor: 5000.00,
+            descricracaoDoPagamento: 'Pagamento da Cloroquina'
+        },
+        {
+            data: '14/11/2023',
+            valor: 100.00,
+            descricracaoDoPagamento: 'Salgado do Donald Trump TALKEY HAHAHA'
+        }
+
+    ]
+
+}
